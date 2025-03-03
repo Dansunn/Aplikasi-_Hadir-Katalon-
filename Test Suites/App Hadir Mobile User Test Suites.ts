c4da75b220e69abcaf8fdd9c04532894db05a708
Dansunn <102135172+Dansunn@users.noreply.github.com>
2025-03-03 21:21:19 +0700

    Add files via upload

---
+++ Test Suites/App Hadir Mobile User Test Suites.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>App Hadir Mobile User Test Suites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8312a3fd-7e85-4739-99aa-33499ac22ff5</testSuiteGuid>
+   <testCaseLink>
+      <guid>23f15d38-ea2b-4427-9a4c-2f975ce5faa9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Koreksi Absen/Positive Test/Koreksi Absen Positive Test Case WFO</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f7bb5450-0f1f-47ad-9626-840b3777d8da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Koreksi Absen/Positive Test/Koreksi Absen Positive Test Case WFH</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>632419e4-7c19-4723-9d41-67676dd20398</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Koreksi Absen/Positive Test/Koreksi Absen Positive Case Test Hanya Jam Masuk</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9179a0c9-9e3c-4125-8031-0163ba0970b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Koreksi Absen/Negatif Test/Koreksi Absen Negative Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8ff2d0c-0f79-4eaa-8c8d-88f7f69a3226</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Terlambat/Negative Test/Izin Terlambat Negative Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5f6cec34-ac04-480f-a685-5469febae929</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Terlambat/Positive Test/Izin Terlambat Positive Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>16a55929-7884-4e7c-ac1d-6fd9b0cd0152</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Pulang Cepat/Negatif Test/Izin Pulang Cepat Negative Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>76888eed-c4a9-4026-a1ad-9ec64d7a8094</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Pulang Cepat/Positive Test/Izin Pulang Cepat Positive Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>034b9962-4d52-4f48-922d-5da0272a78f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Izin Off/Negative Test/Izin Off Negative Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a046b926-c6dc-475a-9d44-c7afed4c2d80</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Modul/Mobile/Izin/Izin Off/Positive Test/Izin Off Positive Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
